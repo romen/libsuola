30549189fcb0923109442cfafe8bb5db838e34bf
Iaroslav Gridin <voker57@gmail.com>
2018-10-08 16:41:42 +0300

    Test against specified debian packages of OpenSSL.
    Unpacks and hacks packages manually. Faster than actual installation but fragile.

--- test/travis/install_deps.sh
+++ test/travis/install_deps.sh
@@ -1,12 +1,41 @@
 set -e
-
 install_openssl() {
 	set -v
 	set -e
 	VERSION="$1"
 	export OSSL_ROOT="$HOME/cache/openssl/$VERSION"
+	
 	if [ -e "$OSSL_ROOT" ]
 	then
+		return 0
+	fi
+	
+	if [ "$VERSION" = native ]
+	then
+		mkdir -p "$HOME/cache/openssl"
+		ln -s /usr "$OSSL_ROOT"
+		return 0
+	fi
+	
+	if [ "$DEBIAN_PACKAGES" ]
+	then
+		TMP="$(mktemp -d)"
+		cd "$TMP"
+		for package in $DEBIAN_PACKAGES
+		do
+			wget "$package" -O package.deb
+			ar x package.deb data.tar.xz
+			mkdir -p data
+			(cd data && tar xf ../data.tar.xz)
+			
+		done
+		
+		# remove multilib prefixes
+		cp -r data/usr/lib/x86_64-linux-gnu/* data/usr/lib/ || :
+		cp -r data/usr/include/x86_64-linux-gnu/* data/usr/include/ || :
+		rm -rf data/usr/include/x86_64-linux-gnu  data/usr/lib/x86_64-linux-gnu
+		mkdir -p "$HOME/cache/openssl"
+		mv data/usr "$OSSL_ROOT"
 		return 0
 	fi
 	
@@ -46,6 +75,14 @@ install_libsodium() {
 }
 
 set -v
+
+if [ "$DEBIAN_PACKAGES" ]
+then
+	# Download the debian packages from link and gut it
+	# dark magic to extract only the checksum
+	DIGEST="$(echo -n "$DEBIAN_PACKAGES" | md5sum | cut -d ' ' -f 1)"
+	export OPENSSL_VERSION="$DIGEST"
+fi
 
 install_openssl "$OPENSSL_VERSION"
 install_libsodium stable
