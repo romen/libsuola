819e99f024777630c9b3b83ab8f7d885461790bd
Nicola Tuveri <nic.tuv@gmail.com>
2019-06-13 21:18:21 +0300

    xenial and arbitrary deb packages for deps
    Co-authored-by: Iaroslav Gridin <voker57@gmail.com>

--- test/travis/build_libsuola.sh
+++ test/travis/build_libsuola.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+
 set -e
 set -v
 cd "$TRAVIS_BUILD_DIR"
 mkdir build
 cd build
-PKG_CONFIG_PATH="$HOME/cache/libsodium/stable/lib/pkgconfig" cmake -DOPENSSL_ROOT_DIR="$OSSL_ROOT" ..
+
+(
+    set -v
+    set -e
+
+    if [ "$LIBSODIUM_VERSION" != "native" ]; then
+        export PKG_CONFIG_PATH="$LIBSODIUM_ROOT/lib/pkgconfig"
+    fi
+    cmake ${OSSL_ROOT:+-DOPENSSL_ROOT_DIR="$OSSL_ROOT"} ..
+)
+
 make
--- test/travis/script.sh
+++
@@ -1,6 +0,0 @@
-set -e
-DN="$(dirname "$0")"
-D="$(readlink -f "$DN")"
-. "$D/install_deps.sh"
-. "$D/build_libsuola.sh"
-. "$D/test_libsuola_unit.sh"
